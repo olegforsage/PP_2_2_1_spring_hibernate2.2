a0326989b5a7fd809c9f8772eba6d563e04f6f96
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:57:48 +0300

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,14 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+    User getUser(String model, int series);
+}
+
